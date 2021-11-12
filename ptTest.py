566bbd5d774ddaf2b62b63b2cadf9c1ceb2bdcda
JB Larson <68449234+JBLarson@users.noreply.github.com>
2021-11-12 10:56:47 -0500

    test priced trades for data completeness

--- ptTest.py
+++ ptTest.py
@@ -3,13 +3,13 @@
 import json
 
 # import kraken data
-sortedTradesInAddr = 'data/pricedTrades.json'
+sortedTradesInAddr = 'data/pricedTrades2.json'
 
 with open(sortedTradesInAddr, 'r') as r:
 	tradeData = json.load(r)
 
 
-buyTrades = tradeData['buy']
+buyTrades, sellTrades = tradeData['buy'], tradeData['sell']
 
 def testData(trade):
 	histQuote = trade['histQuote']
@@ -30,14 +30,45 @@ def testData(trade):
 			if trade['histBase'] != '' and trade['histBase'] != 'histPrice error':
 				missingData = False
 	
-	if missingData != False:
-		print('\n' + str(histPrices))
-		print("There is missing data: " + str(missingData))
-
+	#if missingData != False:
+		#print('\n' + str(histPrices))
+		#print("There is missing data: " + str(missingData))
+	return missingData
 
 #for trade in buyTrades[-20:]:
+
+# metrics for how complete trade data is
+
+
 for trade in buyTrades:
+	missingData = testData(trade)
+	if missingData == None:
+		print('\n')
+		print(trade)
 
-	pass
 
-	#testTradeData = testData(trade)
+for trade in sellTrades:
+	missingData = testData(trade)
+	if missingData == None:
+		print('\n')
+		print(trade)
+
+
+
+
+def missingDataStats():
+	buyTests, sellTests = [], []
+	for trade in buyTrades:
+		testTradeData = testData(trade)
+		buyTests.append(testTradeData)
+
+	for trade in sellTrades:
+		testTradeData = testData(trade)
+		sellTests.append(testTradeData)
+
+
+	missingBuy, missingSell = buyTests.count(None), sellTests.count(None)
+	print("\nBuy trades: " + str(missingBuy) + " out of: " + str(len(buyTrades)))
+	print("Sell trades: " + str(missingSell) + " out of: " + str(len(sellTrades)))
+
+testTrades = missingDataStats()
